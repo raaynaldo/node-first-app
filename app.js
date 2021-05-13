6d55b28bd564315e3437dc0403c48521ea811999
raaynaldo <raaynaldo@hotmail.com>
2021-05-13 15:29:46 -0400

    feat: module explanation

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log(module);
+//module is not global object
+//every file is a module
+// Module {
+//     id: '.',
+//     path: '/Users/Raynaldo/Documents/Code/node/node-first-app',
+//     exports: {},
+//     parent: null,
+//     filename: '/Users/Raynaldo/Documents/Code/node/node-first-app/app.js',
+//     loaded: false,
+//     children: [],
+//     paths: [
+//       '/Users/Raynaldo/Documents/Code/node/node-first-app/node_modules',
+//       '/Users/Raynaldo/Documents/Code/node/node_modules',
+//       '/Users/Raynaldo/Documents/Code/node_modules',
+//       '/Users/Raynaldo/Documents/node_modules',
+//       '/Users/Raynaldo/node_modules',
+//       '/Users/node_modules',
+//       '/node_modules'
+//     ]
+//   }
